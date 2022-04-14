2f3cb2bc7faa29494e7ebea87278d58fdd7d5234
joseluisreyesm <48143883+joseluisreyesm@users.noreply.github.com>
2022-04-13 22:30:03 -0500

    template de publication
    .

---
+++ publication/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from .models import Publication
+
+class PublicationForm(forms.ModelForm):
+    class Meta:
+        model = Publication
+        fields = ('title', 'author', 'description', 'content1', 'content2', 'content3', 'background_pic', 'publication_pic', 'miniature_pic')
+        
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Campo Obligatorio'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Campo Obligatorio'}),
+            'description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Campo Obligatorio'}),
+            'content1': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Campo Obligatorio'}),
+            'content2': forms.Textarea(attrs={'class': 'form-control'}),
+            'content3': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+        
--- publication/views.py
+++ publication/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import PublicationForm
+
 # from publication import Publications
 from publication.models import Publication
 
@@ -12,3 +14,14 @@ def Publicacion(request, id):
     # article = Publication.objects.all()
     # print(public)
     return render(request, 'publicacion.html', context)
+
+def crearPublicacion(request):
+    if request.method == "POST":
+        form = PublicationForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return render(request, 'landing.html')
+    else:
+        form = PublicationForm()
+    return render(request, 'crearPublicacion.html', {'form': form})
