f2ed2ab6c9060588ec393d5e1c31a3b00484b930
CR-MX <31296448+CR-MX@users.noreply.github.com>
2022-10-16 12:05:51 -0500

    Revert "prueba loca"
    This reverts commit 007fd2711a4ea3fd8b5bcf6fe49a2cf794ddde00.

--- educacioncuaz/settings.py
+++ educacioncuaz/settings.py
@@ -137,7 +137,6 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/imagesPublications/'
 
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = '/home/gescacs/PDEC/educacioncuaz/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/imagesPublications')
