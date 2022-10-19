bdc91abc23e87a84928f17c833960ffc1b71587f
CR-MX <ciat117@gmail.com>
2022-10-19 07:38:20 -0500

    Update decorators.py

--- usuarios/decorators.py
+++ usuarios/decorators.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect('eduacionapp:home')
+            return redirect('eduacionapp:adminCenter')
         else:
             return view_func(request, *args, **kwargs)
     return wrapper_func
