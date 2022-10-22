643a0f711a373d312931297fec01ef66437950fd
CR-MX <ciat117@gmail.com>
2022-10-22 01:15:44 -0500

    Update urls.py

--- educacioncuaz/urls.py
+++ educacioncuaz/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, include
 
 from django.conf import settings 
 from django.conf.urls.static import static
+from usuarios import views
 
 urlpatterns = [
+    path('', views.Landing),
     path('admin/', admin.site.urls),
     path('educuaz/publication/', include('publication.urls', namespace='publication_app')),
     path('educuaz/', include('usuarios.urls', namespace='eduacionapp')),
